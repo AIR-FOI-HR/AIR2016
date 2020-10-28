3538cfa1072809ccdae8dbdd02328ea37a509470
domagojmahnet <61117706+domagojmahnet@users.noreply.github.com>
2020-10-28 17:19:52 +0100

    Kreirana početna forma za registraciju

---
+++ app/src/main/java/hr/example/treeapp/RegistrationStep1.java
@@ -0,0 +1,14 @@
+package hr.example.treeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistrationStep1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_step1);
+    }
+}
