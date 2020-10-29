0490402dce46d7e8ce1a7c1f31e95ce29668da75
domagojmahnet <61117706+domagojmahnet@users.noreply.github.com>
2020-10-29 11:24:45 +0100

    Implementiran dizajn drugog koraka registracije, dodane animacije za vraćanje

--- app/src/main/java/hr/example/treeapp/RegistrationStep1.java
+++ app/src/main/java/hr/example/treeapp/RegistrationStep1.java
@@ -13,8 +13,14 @@ public class RegistrationStep1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_step1);
     }
+
     public void OpenRegistrationStep2(View view) {
         Intent open = new Intent(RegistrationStep1.this, RegistrationStep2.class);
         startActivity(open);
+    }
+    public void OpenLogIn(View view){
+        Intent open = new Intent(RegistrationStep1.this, MainActivity.class);
+        startActivity(open);
+        overridePendingTransition(R.anim.slideleft,R.anim.stayinplace);
     }
 }
--- app/src/main/java/hr/example/treeapp/RegistrationStep2.java
+++ app/src/main/java/hr/example/treeapp/RegistrationStep2.java
@@ -13,8 +13,18 @@ public class RegistrationStep2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_step2);
     }
+    public void OpenRegistrationStep1(View view) {
+        Intent open = new Intent(RegistrationStep2.this, RegistrationStep1.class);
+        startActivity(open);
+        overridePendingTransition(R.anim.slideleft,R.anim.stayinplace);
+    }
     public void OpenRegistrationStep3(View view) {
         Intent open = new Intent(RegistrationStep2.this, RegistrationStep3.class);
         startActivity(open);
+    }
+    public void OpenLogIn(View view){
+        Intent open = new Intent(RegistrationStep2.this, MainActivity.class);
+        startActivity(open);
+        overridePendingTransition(R.anim.slideleft,R.anim.stayinplace);
     }
 }
