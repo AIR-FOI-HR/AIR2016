69bb266eb2e249023385c7283732902e0d22df23
domagojmahnet <61117706+domagojmahnet@users.noreply.github.com>
2020-10-29 21:41:11 +0100

    Završen dizajn trećeg koraka registracije

--- app/src/main/java/hr/example/treeapp/RegistrationStep3.java
+++ app/src/main/java/hr/example/treeapp/RegistrationStep3.java
@@ -2,7 +2,9 @@ package hr.example.treeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RegistrationStep3 extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class RegistrationStep3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_step3);
+    }
+    public void OpenRegistrationStep1(View view) {
+        Intent open = new Intent(RegistrationStep3.this, RegistrationStep1.class);
+        startActivity(open);
+        overridePendingTransition(R.anim.slideleft,R.anim.stayinplace);
+    }
+    public void OpenRegistrationStep2(View view) {
+        Intent open = new Intent(RegistrationStep3.this, RegistrationStep2.class);
+        startActivity(open);
+    }
+    public void OpenLogIn(View view){
+        Intent open = new Intent(RegistrationStep3.this, MainActivity.class);
+        startActivity(open);
+        overridePendingTransition(R.anim.slideleft,R.anim.stayinplace);
     }
 }
