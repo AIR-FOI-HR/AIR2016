65cd19b0bf368c920e6bfbc451ef79449b254783
silvio931 <58565990+silvio931@users.noreply.github.com>
2020-10-29 20:52:26 +0100

    Kreirana registracija korisnika, dio unos u Firebase u Authentication

--- app/src/main/java/hr/example/treeapp/RegistrationStep1.java
+++ app/src/main/java/hr/example/treeapp/RegistrationStep1.java
@@ -7,29 +7,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-<<<<<<< HEAD
-=======
+
 
 import com.google.firebase.auth.FirebaseAuth;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
->>>>>>> 512dae518b1901f8ae4a72739a40f614df43328c
+
 
 public class RegistrationStep1 extends AppCompatActivity {
     EditText name, surname, date;
 
-
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_step1);
 
-<<<<<<< HEAD
-    }
-=======
+        firebaseAuth = FirebaseAuth.getInstance();
+
+
+    //}
+
       //  name=findViewById(R.id.txtBoxStep1Name);
        // surname=findViewById(R.id.txtBoxStep1Surname);
         //btStep2=findViewById(R.id.btnStep1Next);
@@ -46,7 +47,7 @@ public class RegistrationStep1 extends AppCompatActivity {
 
         }
 
->>>>>>> 512dae518b1901f8ae4a72739a40f614df43328c
+
 
 
     public void OpenRegistrationStep2(View view) {
@@ -62,7 +63,9 @@ public class RegistrationStep1 extends AppCompatActivity {
         open.putExtra("name_key", name1);
         open.putExtra("surname_key", surname1);
    //     open.putExtra("date_key", date1);
+
         startActivity(open);
+
     }
     public void OpenLogIn(View view){
         Intent open = new Intent(RegistrationStep1.this, MainActivity.class);
@@ -70,8 +73,7 @@ public class RegistrationStep1 extends AppCompatActivity {
         overridePendingTransition(R.anim.slideleft,R.anim.stayinplace);
     }
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 512dae518b1901f8ae4a72739a40f614df43328c
+
+
 }
--- app/src/main/java/hr/example/treeapp/RegistrationStep2.java
+++ app/src/main/java/hr/example/treeapp/RegistrationStep2.java
@@ -1,19 +1,29 @@
 package hr.example.treeapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.protobuf.DescriptorProtos;
 
 import org.w3c.dom.Text;
 
 public class RegistrationStep2 extends AppCompatActivity {
+    EditText email, korIme, password, repeatedPassword;
+
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +38,15 @@ public class RegistrationStep2 extends AppCompatActivity {
         Log.d("Poruka", str_surname);
     //    Log.d("Poruka", str_date);
 
+        //dohvacanje upisanih podataka kao objekte
+        email = (EditText)findViewById(R.id.txtBoxStep2Email);
+        korIme = (EditText)findViewById(R.id.txtBoxStep2Username);
+        password = (EditText)findViewById(R.id.txtBoxStep2Password);
+        repeatedPassword = (EditText)findViewById(R.id.txtBoxStep2PasswordRepeat);
+
+        //kreiranje instance Firebase autentikacije
+        firebaseAuth = FirebaseAuth.getInstance();
+
     }
     public void OpenRegistrationStep1(View view) {
         Intent open = new Intent(RegistrationStep2.this, RegistrationStep1.class);
@@ -35,8 +54,50 @@ public class RegistrationStep2 extends AppCompatActivity {
         overridePendingTransition(R.anim.slideleft,R.anim.stayinplace);
     }
     public void OpenRegistrationStep3(View view) {
-        Intent open = new Intent(RegistrationStep2.this, RegistrationStep3.class);
-        startActivity(open);
+        //kreiranje stringova upisanih podataka
+        String Email = email.getText().toString().trim();
+        String KorIme = korIme.getText().toString().trim();
+        String Password = password.getText().toString().trim();
+        String RepeatedPassword = repeatedPassword.getText().toString().trim();
+
+        //provjerava je li upisan e-mail
+        if(TextUtils.isEmpty(Email)){
+            email.setError("Upišite važeći e-mail.");
+            return;
+        }
+
+        //provjerava je li upisana lozinka
+        if(TextUtils.isEmpty(Password)){
+            password.setError("Unesite lozinku!");
+            return;
+        }
+
+        //provjerava sadrži li lozinka najmanje 6 znakova
+        if(Password.length() < 6){
+            password.setError("Lozinka mora sadržavati najmanje 6 znakova.");
+            return;
+        }
+
+        //provjerava je li točno upisana ponovljena lozinka
+        if(TextUtils.isEmpty(RepeatedPassword) || !RepeatedPassword.equals(Password)){
+            repeatedPassword.setError("Ponovite lozinku.");
+            return;
+        }
+
+        //upisuje korisnika u Authentication i Database kolekciju korisnici ukoliko nema pogreške
+        firebaseAuth.createUserWithEmailAndPassword(Email, Password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Intent open = new Intent(RegistrationStep2.this, RegistrationStep3.class);
+                    startActivity(open);
+                }else{
+
+                }
+            }
+        });
+
+
     }
     public void OpenLogIn(View view){
         Intent open = new Intent(RegistrationStep2.this, MainActivity.class);
