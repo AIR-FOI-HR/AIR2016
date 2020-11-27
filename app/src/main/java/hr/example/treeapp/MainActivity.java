b5d33edc38d887c23df7578f4d952ce71724e4d2
domagojmahnet <61117706+domagojmahnet@users.noreply.github.com>
2020-11-27 21:16:23 +0100

    Premješten SplashScreen

--- app/src/main/java/hr/example/treeapp/MainActivity.java
+++ app/src/main/java/hr/example/treeapp/MainActivity.java
@@ -1,46 +1,19 @@
 package hr.example.treeapp;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.SystemClock;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-import java.util.concurrent.Executor;
 
 import auth.AuthRepository;
 import auth.LogInStatusCallback;
+import auth.PassReset;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -117,7 +90,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void OpenReset(View view){
-        Intent open= new Intent(MainActivity.this,PassReset.class);
+        Intent open= new Intent(MainActivity.this, PassReset.class);
         startActivity(open);
     }
 
