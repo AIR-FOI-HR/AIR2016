4461626b2efdb0543ec77240bc6cb149cfef16e5
domagojmahnet <61117706+domagojmahnet@users.noreply.github.com>
2020-11-27 20:48:52 +0100

    Obrisani ostaci callbacka

--- app/src/main/java/hr/example/treeapp/RegistrationStep3.java
+++ app/src/main/java/hr/example/treeapp/RegistrationStep3.java
@@ -1,23 +1,13 @@
 package hr.example.treeapp;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-import auth.CurrentUserCallback;
 import auth.RegistrationRepository;
-import auth.UsernameAvailabilityCallback;
 
 
 public class RegistrationStep3 extends AppCompatActivity {
