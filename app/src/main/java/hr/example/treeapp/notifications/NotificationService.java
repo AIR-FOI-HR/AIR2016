1afa24f9f06e62ee0232b19227f818556d8e57a3
leon-mir <mircic.leon@gmail.com>
2021-01-13 23:43:30 +0100

    Implementirano otvaranje aktivnosti notifikacija prilikom klika na notifikaciju

--- app/src/main/java/hr/example/treeapp/notifications/NotificationService.java
+++ app/src/main/java/hr/example/treeapp/notifications/NotificationService.java
@@ -4,19 +4,18 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.Service;
 import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
-import android.media.RingtoneManager;
 import android.os.Build;
+import android.os.IBinder;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
 
-import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -25,20 +24,22 @@ import java.util.Map;
 import hr.example.treeapp.R;
 import hr.example.treeapp.notifications.NotificationsActivity;
 
-public class MyFirebaseMessagingService extends FirebaseMessagingService {
-    private static final String TAG = "Notification";
-
-    @Override
-    public void onNewToken(String token) {
-        Log.d(TAG, "Refreshed token: " + token);
+public class NotificationService extends FirebaseMessagingService {
+    public NotificationService() {
     }
 
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
+        Log.d("a","tu sam");
         if(remoteMessage.getData().size()>0){
             sendNotification2(remoteMessage);
         }
+    }
+
+    @Override
+    public void onDeletedMessages() {
+        super.onDeletedMessages();
     }
 
     private void sendNotification2(RemoteMessage remoteMessage) {
@@ -50,14 +51,15 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
                 | Intent.FLAG_ACTIVITY_CLEAR_TASK);
 
-        PendingIntent pendingIntent =
-                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        /*PendingIntent pendingIntent =
+                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);*/
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent, PendingIntent.FLAG_ONE_SHOT);
 
         Map<String,String> data = remoteMessage.getData();
         String title = data.get("title");
         String content = data.get("content");
         String activity = data.get("activity");
-
+        Log.d("a","tu sam");
         NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
         String NOTIFICATION_CHANNEL_ID = "11";
         if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.O){
@@ -84,46 +86,5 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         notificationManager.notify(1, notificationBuilder.build());
 
 
-    }
-
-    private void sendNotification(String messageBody, String id, String clickAction) {
-        Intent intent = new Intent(this, NotificationsActivity.class);
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-        stackBuilder.addNextIntentWithParentStack(intent);
-        //intent.putExtra("id", id);
-        //intent.putExtra("body", messageBody);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
-                | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-
-        PendingIntent pendingIntent =
-                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, "111")
-                .setSmallIcon(R.drawable.default_image)
-                .setContentText(messageBody)
-                .setAutoCancel(true)
-                .setVibrate(new long[]{1000, 1000, 1000, 1000, 1000})
-                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
-                .setContentIntent(pendingIntent)
-                .setLights(Color.GREEN, 3000, 3000);
-
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            NotificationManager notificationManager =
-                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-            int importance = NotificationManager.IMPORTANCE_HIGH;
-            NotificationChannel notificationChannel = new NotificationChannel("111", "NOTIFICATION_CHANNEL_NAME", importance);
-            notificationChannel.enableLights(true);
-            notificationChannel.setLightColor(Color.RED);
-            notificationChannel.enableVibration(true);
-            notificationChannel.setShowBadge(false);
-            notificationChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
-            assert notificationManager != null;
-            notificationBuilder.setChannelId("111");
-            notificationManager.createNotificationChannel(notificationChannel);
-            notificationManager.notify(0, notificationBuilder.build());
-        } else {
-            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-            notificationManager.notify(0, notificationBuilder.build());
-        }
     }
 }
